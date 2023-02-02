520ccb8459bdcf3252ea322d1929024da15bb785
Kaushik Gnanasekar <gnanasekar.ka@northeastern.edu>
2023-02-01 20:46:41 -0500

    minor fixes

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes/index.js';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(cors());
+app.use(morgan('combined'));
 app.use(express.json()); // Parse json
 app.use(express.urlencoded({ extended: true }));
 //Load routes
