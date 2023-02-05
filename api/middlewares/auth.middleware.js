efa11896d4eebaf41b1c39427b7feb79ac38cdcb
Kaushik Gnanasekar <gnanasekar.ka@northeastern.edu>
2023-02-05 15:47:39 -0500

    auth middleware minor fix

--- api/middlewares/auth.middleware.js
+++ api/middlewares/auth.middleware.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import UserService from '../services/user.service.js';
 import responseHandler from '../utils/responseHandler.js';
 import base64Util from '../utils/base64.js';
+import authUtil from '../utils/authUtil.js';
 
 /**
  * Basic Auth Middleware
@@ -9,7 +10,7 @@ import base64Util from '../utils/base64.js';
  * Authorization: Basic <username:password>
  */
 const authMiddleware = () => async (req, res, next) => {
-    const basicAuth = _.get(req, 'headers.authorization');
+    const basicAuth = authUtil.extractAsBasicAuth(_.get(req, 'headers.authorization'));
     const id = _.get(req, 'params.userId');
     if (_.isEmpty(basicAuth)) return responseHandler(res, 'Unauthorized', 401);
     const [username, password] = base64Util.decode(basicAuth).split(':');
