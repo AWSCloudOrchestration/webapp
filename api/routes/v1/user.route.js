97e9fea7d59f4b990d4f0194b7e62d28ac9b88e1
Kaushik Gnanasekar <gnanasekar.ka@northeastern.edu>
2023-02-05 16:31:19 -0500

    added validation middleware, user api validations, minor fixes

--- api/routes/v1/user.route.js
+++ api/routes/v1/user.route.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { createUser, getUserInfo, updateUser } from '../../controllers/user.controller.js';
 import authMiddleware from '../../middlewares/auth.middleware.js';
+import requestValidationMiddleware from '../../middlewares/validation.middleware.js';
+import { getUserById, update } from '../../validations/v1/user.validations.js';
 
 const router = express.Router();
 
@@ -10,7 +12,7 @@ router
 
 router
     .route('/:userId')
-    .get(authMiddleware(), getUserInfo)
-    .put(authMiddleware(), updateUser);
+    .get(authMiddleware(), requestValidationMiddleware(getUserById), getUserInfo)
+    .put(authMiddleware(), requestValidationMiddleware(update), updateUser);
 
 export default router;
