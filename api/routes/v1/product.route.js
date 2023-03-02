60058a0eb581154e9085a482ea6b01735ea91a9e
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2023-03-02 10:37:10 -0500

    upload storage changed from memory to disk

--- api/routes/v1/product.route.js
+++ api/routes/v1/product.route.js
@@ -6,13 +6,13 @@ import { get, create, update, deleteProduct, patch,
 import authMiddleware from '../../middlewares/auth.middleware.js';
 import requestValidationMiddleware from '../../middlewares/validation.middleware.js';
 import multer from 'multer';
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
 import { getProductById,
   updateProduct,
   createProduct,
   patchProduct,
   deleteByProductId } from '../../validations/v1/product.validations.js';
+// Multer
+const upload = multer({ dest: '/tmp/uploads' });
 
 const router = express.Router();
 
--- api/validations/v1/product.validations.js
+++ api/validations/v1/product.validations.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 const getProductById = {
   params: Joi.object().keys({
-    productId: Joi.string().required(),
+    productId: Joi.number().integer().required(),
   }),
 };
 
@@ -12,39 +12,39 @@ const createProduct = {
     description: Joi.string().required(),
     sku: Joi.string().required(),
     manufacturer: Joi.string().required(),
-    quantity: Joi.number().integer().min(0).max(100).required(),
+    quantity: Joi.number().integer().min(0).max(100).prefs({ convert: false }).required(),
   }),
 };
 
 const updateProduct = {
   params: Joi.object().keys({
-    productId: Joi.string().required(),
+    productId: Joi.number().integer().required(),
   }),
   body: Joi.object().keys({
     name: Joi.string().required(),
     description: Joi.string().required(),
     sku: Joi.string().required(),
     manufacturer: Joi.string().required(),
-    quantity: Joi.number().integer().min(0).max(100).required(),
+    quantity: Joi.number().integer().min(0).max(100).prefs({ convert: false }).required(),
   }),
 };
 
 const patchProduct = {
   params: Joi.object().keys({
-    productId: Joi.string().required(),
+    productId: Joi.number().integer().required(),
   }),
   body: Joi.object().keys({
     name: Joi.string(),
     description: Joi.string(),
     sku: Joi.string(),
     manufacturer: Joi.string(),
-    quantity: Joi.number().integer().min(0).max(100).required(),
+    quantity: Joi.number().integer().min(0).max(100).prefs({ convert: false }).required(),
   }),
 };
 
 const deleteByProductId = {
   params: Joi.object().keys({
-    productId: Joi.string().required(),
+    productId: Joi.number().integer().required(),
   }),
 };
 
