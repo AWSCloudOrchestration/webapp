5b1a649f5311cd8d22909084759e06c38fd80b11
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2023-03-16 12:11:54 -0400

    env name changes

--- db/sql/migrations/config/config.js
+++ db/sql/migrations/config/config.js
@@ -2,17 +2,17 @@ import 'dotenv/config';
 
 export default {
   'development': {
-    'username': process.env.DEV_SQL_USER,
-    'password': process.env.DEV_SQL_PASS,
-    'database': process.env.DEV_SQL_DB,
-    'host': process.env.DEV_SQL_HOST,
+    'username': process.env.SQL_USER,
+    'password': process.env.SQL_PASS,
+    'database': process.env.SQL_DB,
+    'host': process.env.SQL_HOST,
     'dialect': 'mysql',
   },
   'production': {
-    'username': process.env.PROD_SQL_USER,
-    'password': process.env.PROD_SQL_PASS,
-    'database': process.env.PROD_SQL_DB,
-    'host': process.env.PROD_SQL_HOST,
+    'username': process.env.SQL_USER,
+    'password': process.env.SQL_PASS,
+    'database': process.env.SQL_DB,
+    'host': process.env.SQL_HOST,
     'dialect': 'mysql',
   },
 };
