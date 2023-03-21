8788ad1456bdc8a35bd273b3d38229a259159a11
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2023-03-21 16:22:22 -0400

    metric middleware changes, script changes for log permissions

--- api/middlewares/metrics.middleware.js
+++ api/middlewares/metrics.middleware.js
@@ -1,9 +1,17 @@
 import { statsDClient } from '../../statsd/index.js';
 import logger from '../../logger/index.js';
 
+/**
+ * API calls counter
+ * @param {String} method
+ * @param {String} url
+ */
 const countAllApiCalls = (method, url) => {
-  const metricName = `.${method.toLowerCase()}.${url}.count`;
-  statsDClient.increment(metricName);
+  const statsPrefix = process.env.STATSD_PREFIX;
+  let metricName = `_${method.toLowerCase()}_${url}_count`;
+  if (!statsPrefix) metricName = metricName.slice(1);
+  const generalizedName = metricName.replace(/\/\d+/g, '{id}');
+  statsDClient.increment(generalizedName);
 };
 
 const metricsMiddleware = (req, res, next) => {
--- scripts/setup-node-env.sh
+++ scripts/setup-node-env.sh
@@ -19,6 +19,20 @@ sudo bash -c 'cat > /opt/cloudwatch-config.json <<EOF
       "metrics_collection_interval": 10,
       "logfile": "/var/logs/amazon-cloudwatch-agent.log"
   },
+  "logs": {
+      "logs_collected": {
+          "files": {
+              "collect_list": [
+                  {
+                      "file_path": "/var/log/webapp/*.log",
+                      "log_group_name": "csye6225",
+                      "log_stream_name": "webapp"
+                  }
+              ]
+          }
+      },
+      "log_stream_name": "cloudwatch_log_stream"
+  },
   "metrics":{
     "namespace":"WebappMetrics",
     "metrics_collected":{
@@ -50,6 +64,11 @@ mkdir /home/ec2-user/webapp
 tar -xf /home/ec2-user/webapp.tar.gz -C /home/ec2-user/webapp
 # npm install
 npm install --omit=dev --prefix /home/ec2-user/webapp
+
+# Create log folder with permissions
+sudo mkdir -p /var/log/webapp
+sudo chown -R ec2-user:ec2-user /var/log/webapp
+sudo chmod -R ug+rw /var/log/webapp
 
 # Create systemd service
 sudo touch /etc/systemd/system/webapp.service
