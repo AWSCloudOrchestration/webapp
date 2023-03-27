46163e4bb811416b49929fcc92f6f6b16f229bb6
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2023-03-27 19:37:49 -0400

    graceful shutdown added

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 import app from './api/app.js';
-import { initSqlConn } from './db/sql/index.js';
-import { initStatsDClient } from './statsd/index.js';
+import { initSqlConn, closeSqlConn } from './db/sql/index.js';
+import { initStatsDClient, closeStatsDClient } from './statsd/index.js';
 import logger from './logger/index.js';
+import centralErrorHandler from './api/utils/centralErrorHandler.js';
 import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
 
 const port = process.env.APP_PORT || 3000;
 
-app.listen(port, async () => {
+const server = app.listen(port, async () => {
   await initSqlConn();
   initStatsDClient();
-  console.log('Server listening on port', port);
   logger.info(`Server listening on port ${port}`);
 });
+
+const gracefulShutdown = () => {
+  if (server) {
+    server.close(async (err) => {
+      if (err) {
+        logger.error('Shutting down with error: ', { error: err.stack });
+        process.exitCode = 1;
+      }
+      await closeSqlConn();
+      closeStatsDClient();
+      process.exit();
+    });
+  }
+};
+
+process.on('uncaughtException', (error) => {
+  centralErrorHandler(error);
+});
+
+process.on('unhandledRejection', (reason) => {
+  centralErrorHandler(reason);
+});
+
+process.on('SIGINT', () => {
+  logger.info('SIGINT received, gracefully shutting down.');
+  gracefulShutdown();
+});
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received, gracefully shutting down.');
+  gracefulShutdown();
+});
+
